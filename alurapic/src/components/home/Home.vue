16177cd0d8e6f6d5c04252493d6fe27b31e1890d
fabriciofs <fabricio@fabriciofs.eti.br>
2017-03-14 15:29:40 -0300

    first commit

---
+++ alurapic/src/components/home/Home.vue
@@ -0,0 +1,113 @@
+
+<template>
+    <div>
+        <h1 class="centralizado">{{titulo}}</h1>
+        <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
+        <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre por parte do título">
+        <ul class="lista-fotos">
+            <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+                <meu-painel :titulo="foto.titulo">
+                    <imagem-responsiva v-meu-transform:scale.animate.reverse="1.2" :url="foto.url" :titulo="foto.titulo" />
+                    <router-link :to="{ name: 'altera', params: { id: foto._id } }">
+                        <meu-botao tipo="button" rotulo="ALTERAR" estilo="padrao"/>
+                    </router-link>
+                    <meu-botao tipo="button" rotulo="REMOVER" :confirmacao="true" @botaoAtivado="remove($event, foto)" estilo="perigo"/>
+                </meu-painel>
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+    import Painel from '../shared/painel/Painel.vue';
+    import Botao from '../shared/botao/Botao.vue';
+    import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+    import transform from '../../directives/Transform';
+    import FotoService from '../../domain/foto/FotoService';
+
+    export default {
+
+        components: {
+            'meu-painel' : Painel,
+            'imagem-responsiva': ImagemResponsiva,
+            'meu-botao' : Botao
+        },
+
+        directives: {
+            'meu-transform': transform
+        },
+
+        computed: {
+            fotosComFiltro() {
+
+                if(this.filtro) {
+                    let exp = new RegExp(this.filtro.trim(), 'i');
+                    return this.fotos.filter(foto => exp.test(foto.titulo));
+                    return [];
+                } else {
+                    return this.fotos;
+                }
+            }
+        },
+
+        data() {
+            return {
+                titulo: 'Alurapic',
+                fotos: [],
+                filtro: '',
+                mensagem: ''
+            }
+        },
+
+        methods: {
+
+            remove($event, foto) {
+
+                this.service.apaga(foto._id)
+                    .then(() => {
+                        let indice = this.fotos.indexOf(foto);
+                        this.fotos.splice(indice, 1);
+                        this.mensagem = "Foto removida com sucesso";
+                    }, err => {
+                        this.mensagem = err.message;
+                    });
+            },
+
+            altera($event, foto) {
+
+            }
+
+        },
+
+        created() {
+
+            this.service = new FotoService(this.$resource);
+            this.service.lista()
+                .then(fotos => this.fotos = fotos, err => {
+                    this.mensagem = err.message;
+                });
+        }
+    }
+</script>
+
+<style>
+
+    .centralizado {
+        text-align: center;
+    }
+
+    .lista-fotos {
+        list-style: none;
+    }
+
+    .lista-fotos .lista-fotos-item {
+        display: inline-block;
+    }
+
+    .filtro {
+        display: block;
+        width: 100%;
+    }
+
+</style>
---
+++ alurapic/src/main.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
+import VeeValidate from 'vee-validate';
+import msg from './pt_Br';
+
+Vue.use(VueResource);
+Vue.http.options.root = 'http://localhost:3000';
+
+Vue.use(VueRouter);
+
+Vue.use(VeeValidate, {
+    locale: 'pt_BR',
+    dictionary: {
+        pt_BR: {
+            messages: msg
+        }
+    }
+});
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
